41ac843c77315ca0a60416ab40f1616db3fe66b5
owch <bowenrainyday@gmail.com>
2016-10-16 01:59:51 -0400

    list v1

--- routes/index.js
+++ routes/index.js
@@ -10,6 +10,69 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.post('/v1/user', function(req, res, next) {
+  var formdata = req.body;
+  var path = req.originalUrl;
+  var url = base_url + path;
+
+  var userName = formdata.username;
+  var password = formdata.password;
+
+  var data = {
+    "username": userName,
+    "password": password
+  }
+
+  request({
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8'
+    },
+    uri: url,
+    body: JSON.stringify(data),
+    method: 'POST'
+  }, function (err, res2, body) {
+    if(!err) {
+      console.log("Success");
+      res.writeHead(200, {'Content-Type': 'text/plain'});
+      res.end(body);
+    }
+    else {
+      console.log("Failed");
+      res.status(500).send(err);
+    }
+  });
+
+});
+
+
+router.post('/v1/list/2', function(req, res, next) {
+  var formdata = req.body;
+  var path = req.originalUrl;
+  var url = base_url + path;
+
+  var token = 'Bearer ' + formdata.token;
+
+  request({
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8',
+      'Authorization' : token
+    },
+    uri: url,
+    method: 'GET'
+  }, function (err, res2, body) {
+    if(!err) {
+      console.log("Success");
+      res.writeHead(200, {'Content-Type': 'text/plain'});
+      res.end(body);
+    }
+    else {
+      console.log("Failed");
+      res.status(500).send(err);
+    }
+
+  });
+});
+
 router.post('/v1/user/create', function(req, res, next) {
   var formdata = req.body;
   var path = req.originalUrl;
